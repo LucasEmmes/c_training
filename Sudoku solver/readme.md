bfca6fd7c15b349a1454869e795bfc80bab23db1
Lucas Emmes <79790657+LucasEmmes@users.noreply.github.com>
2021-10-06 20:07:28 +0200

    Sudoku solver now works!
    Finally lmao

---
+++ Sudoku solver/readme.md
@@ -0,0 +1,5 @@
+# Sudoku solver  
+This is a basic sudoku solver which uses recursion  
+First time I wrote it I based it off an earlier solver I made  
+in python, except it somehow turned out that one never worked ._.  
+Well this one does, anyway...
--- Sudoku solver/sudoku.c
+++ Sudoku solver/sudoku.c
@@ -1,201 +1,233 @@
-#include <stdio.h>
 #include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-char total, r, nx, ny, x, y, max;
-char c[2];
+char grid_coordinates[2];
 
-void coord(char pos) {
-    c[0] = pos % 9;
-    c[1] = pos / 9;
-}
-
-void print_array(char arr[][9]) {
-    for (char i = 0; i < 9; i++)
+/* Takes in two lists and their size. Compares the first <size> elements
+ * Returns 1/0 for True/False
+ */
+char compare_lists(char a[], char b[], char size) {
+    for (char i = 0; i < size; i++)
     {
-        for (char j = 0; j < 9; j++)
-        {
-            printf("%d ", arr[i][j]);
+        if (a[i] != b[i]) {
+            return 0;
         }
-        
-        printf("\n");
     }
-    
+    return 1;
+}
+void compare_lists_test() {
+    char x[] = {1,2,3};
+    char y[] = {1,2,3};
+    char z[] = {3,2,1};
+    assert(compare_lists(x, y, 3));
+    assert(!compare_lists(z, y, 3));
 }
 
-void check_row(char sudoku[][9], char row, char allowed[]) {
+
+/* Takes in a position on the grid (global cell number) and
+ * converts it into cartesian coordinates (both 0-indexed)
+ * RETURNS: List[x, y]
+ */
+char* get_coordinates(char position) {
+    grid_coordinates[0] = position % 9;
+    grid_coordinates[1] = position / 9;
+    return grid_coordinates;
+}
+void  get_cooridinates_test() {
+    assert(get_coordinates(80)[0] == 8);
+    assert(get_coordinates(80)[1] == 8);
+    assert(get_coordinates(79)[0] == 7);
+    assert(get_coordinates(79)[1] == 8);
+}
+
+
+/* Takes a list of allowed numbers and removes any new illegal numbers it finds
+ * in the same row as <position>. Modifes list in-place
+ */
+void check_row(char allowed[], char sudoku[][9], char position) {
+    char row = get_coordinates(position)[1];
     for (char i = 0; i < 9; i++)
     {
-        if (sudoku[row][i]) {
+        if (sudoku[row][i])
+        {
             allowed[sudoku[row][i]-1] = 0;
         }
     }
 }
-
-void check_col(char sudoku[][9], char col, char allowed[]) {
+void check_row_test() {
+    // Generate 1 row of sudoku
+    char s[1][9];
     for (char i = 0; i < 9; i++)
     {
-        if (sudoku[i][col]) {
-            allowed[sudoku[i][col]-1] = 0;
+        s[0][i] = i+1;
+    }
+    // Set up allowed list
+    char allowed[9] = {1,1,1,1,1,1,1,1,1};
+
+    // Check if 1 is allowed
+    s[0][0] = 0;
+    char fasit1[9] = {1,0,0,0,0,0,0,0,0};
+    check_row(allowed, s, 0);
+    assert(compare_lists(allowed, fasit1, 9));
+
+    // Check if none are allowed
+    s[0][0] = 1;
+    char fasit2[9] = {0,0,0,0,0,0,0,0,0};
+    check_row(allowed, s, 0);
+    assert(compare_lists(allowed, fasit2, 9));
+}
+
+/* Takes a list of allowed numbers and removes any new illegal numbers it finds
+ * in the same column as <position>. Modifes list in-place
+ */
+void check_column(char allowed[], char sudoku[][9], char position) {
+    char column = get_coordinates(position)[0];
+    for (char i = 0; i < 9; i++)
+    {
+        if (sudoku[i][column])
+        {
+            allowed[sudoku[i][column]-1] = 0;
         }
     }
 }
-
-void check_square(char sudoku[][9], char pos, char allowed[]) {
-    coord(pos);
-    char dx = (c[0]/3)*3;
-    char dy = (c[1]/3)*3;
-    for (char i = 0; i < 3; i++)
-    {
-        for (char j = 0; j < 3; j++)
-        {
-            if (sudoku[dy+i][dx+j]) {
-                allowed[sudoku[dy+i][dx+j]-1] = 0;
-            }
-        }
-    }
-}
-
-char compare(char l1[], char l2[], char size) {
-    for (char i = 0; i < size; i++)
-    {
-        if (l1[i] != l2[i])
-        {
-            return 0;
-        }
-        
-    }
-    return 1;
-}
-
-char valid(char sudoku[][9]) {
-    // Check rows
+void check_column_test() {
+    // Generate 1 row of sudoku
+    char s[9][9];
     for (char i = 0; i < 9; i++)
     {
-        char taken[] = {0,0,0,0,0,0,0,0,0};
-        for (char j = 0; j < 9; j++)
-        {
-            if (sudoku[i][j]) {
-                if (taken[sudoku[i][j]-1]) {
-                    return 0;
-                } else {
-                    taken[sudoku[i][j]-1]++;
-                }
-            }
-        }
+        s[i][0] = i+1;
     }
+    // Set up allowed list
+    char allowed[9] = {1,1,1,1,1,1,1,1,1};
 
-    // Check cols
-    for (char i = 0; i < 9; i++)
-    {
-        char taken[] = {0,0,0,0,0,0,0,0,0};
-        for (char j = 0; j < 9; j++)
-        {
-            if (sudoku[j][i]) {
-                if (taken[sudoku[j][i]-1]) {
-                    return 0;
-                } else {
-                    taken[sudoku[j][i]-1]++;
-                }
-            }
-        }
-    }
+    // Check if 1 is allowed
+    s[0][0] = 0;
+    char fasit1[9] = {1,0,0,0,0,0,0,0,0};
+    check_column(allowed, s, 0);
+    assert(compare_lists(allowed, fasit1, 9));
 
-    // Check squares
-    for (char x = 0; x < 9; x++)
+    // Check if none are allowed
+    s[0][0] = 1;
+    char fasit2[9] = {0,0,0,0,0,0,0,0,0};
+    check_column(allowed, s, 0);
+    assert(compare_lists(allowed, fasit2, 9));
+}
+
+/* Takes a list of allowed numbers and removes any new illegal numbers it finds
+ * in the same square as <position>. Modifes list in-place
+ */
+void check_square(char allowed[], char sudoku[][9], char position) {
+    char square_x = (get_coordinates(position)[0]/3)*3;
+    char square_y = (get_coordinates(position)[1]/3)*3;
+
+    for (char y_offset = 0; y_offset < 3; y_offset++)
     {
-        char taken[] = {0,0,0,0,0,0,0,0,0};
-        char tx = (x%3)*3;
-        char ty = (x/3)*3;
-        for (char i = 0; i < 3; i++)
+        for (char x_offset = 0; x_offset < 3; x_offset++)
         {
-            for (char j = 0; j < 3; j++)
+            if (sudoku[square_y + y_offset][square_x + x_offset])
             {
-                if (sudoku[ty+i][tx+j]) {
-                    if (taken[sudoku[ty+i][tx+j]]) {
-                        return 0;
-                    } else {
-                        taken[sudoku[ty+i][tx+j]-1]++;
-                    }
-                }
+                allowed[sudoku[square_y + y_offset][square_x + x_offset]-1] = 0;
             }
         }
     }
-    
-    return 1;
+}
+void check_square_test() {
+    char s[9][9] = {
+        {0,7,8,5,0,0,0,0,0},
+        {0,0,3,0,0,7,8,0,0},
+        {0,0,0,1,9,0,0,0,0},
+        {0,0,7,0,0,0,2,9,0},
+        {0,9,0,0,6,1,0,4,0},
+        {0,0,0,0,0,4,0,0,0},
+        {3,0,6,0,0,2,0,0,0},
+        {0,1,0,0,0,0,0,0,4},
+        {0,0,0,0,0,0,5,0,0}
+        };
+
+    char t1[9] = {1,1,1,1,1,1,1,1,1};
+    char s1[9] = {1,1,0,1,1,1,0,0,1};
+    check_square(t1, s, 0);
+    assert(compare_lists(t1, s1, 9));
+
+    char t3[9] = {1,1,1,1,1,1,1,1,1};
+    char s3[9] = {1,1,1,1,1,1,1,0,1};
+    check_square(t3, s, 26);
+    assert(compare_lists(t3, s3, 9));
 }
 
-char solve(char sudoku[][9], char pos) {
-    // CHECKS
-    // assert(valid(sudoku));
-    if (!valid(sudoku)) {
-        printf("a");
+/* Prints a standard 9x9 sudoku array
+ * Purely for visualization
+ */
+void print_sudoku(char sudoku[][9]) {
+    for (char y = 0; y < 9; y++)
+    {
+        for (char x = 0; x < 9; x++)
+        {
+            printf("%d ", sudoku[y][x]);
+        }
+        printf("\n");
     }
-    if (pos == 4) {
-        printf("b");
-    }
-    
-    
-    
-    
-    if (pos > max) {
-        max = pos;
-    }
-    coord(pos);
-    x = c[0];
-    y = c[1];
- 
-    // If we have solved the sudoku, return 1 downwards
-    if (pos == 81) {
+}
+
+
+/* Recursively attempt to place a valid number in one
+ * cell at a time, backtracking if we hit a dead end.
+ * Solves the soduoku in-place
+ */
+char solve(char sudoku[][9], char position) {
+
+    // If we hit position 81, then we managed to place
+    // a number on every tile a.k.a. we completed it
+    if (position == 81) {
         return 1;
     }
 
-    // If the current spot is taken, we just pass along
+    // If not, then we have some work to do
+
+    // Get current cartesian coordinates
+    char x = get_coordinates(position)[0];
+    char y = get_coordinates(position)[1];
+
+    // If the current cell already has a value: skip
     if (sudoku[y][x]) {
-        return solve(sudoku, pos+1);
+        return solve(sudoku, position+1);
     }
 
-    // List of possible values to check
+    // Set up list to keep track of possible numbers
     char allowed[9] = {1,1,1,1,1,1,1,1,1};
 
-    // check row
-    check_row(sudoku, y, allowed);
-    // check col
-    check_col(sudoku, y, allowed);
-    // check block
-    check_square(sudoku, pos, allowed);
-    
-    // So that we may return 0 if there are not valid options
-    char empty = 1;
+    // Eliminate all dissalowed values
+    check_row(allowed, sudoku, position);
+    check_column(allowed, sudoku, position);
+    check_square(allowed, sudoku, position);
 
-    // Iterate over options and test if available
+    // Go through every number 1-9 and check if it was available
     for (char i = 0; i < 9; i++)
     {
         if (allowed[i]) {
-            empty = 0;
-            // Modify sudoku
+            // If a number is available, we test it
             sudoku[y][x] = i+1;
-
-            // Recursively call
-            r = solve(sudoku, pos+1);
-            // If we solved it, return downwards
-            if (r) {
-                return r;
+            char result = solve(sudoku, position+1);
+            // If we get back 1, then we know we completed it
+            if (result) {
+                return 1;
             }
         }
     }
-    
-    // If we end up here, then either
-    // a: there were no options available, or
-    // b: no option helped solve the sudoku
+
+
+    // If none of the available options worked,
+    // then we reset this cell and go back
     sudoku[y][x] = 0;
     return 0;
-    
+
 }
 
 
+int main(int argc, char const *argv[]) {
 
-int main(int argc, char const *argv[])
-{
+    // Pain and suffering
 
     char sudoku_base[9][9] = {
         {0,7,8,5,0,0,0,0,0},
@@ -209,35 +241,9 @@ int main(int argc, char const *argv[])
         {0,0,0,0,0,0,5,0,0}
         };
 
-    // Correct result
-    // 1 7 8 | 7 8 5 | 8 5 2
-    // 2 5 9 | 5 9 4 | 9 4 7
-    // 4 6 3 | 6 3 1 | 3 1 9
-    // - - - - - - - - - - -
-    // 5 4 1 | 4 1 3 | 1 3 8
-    // 8 9 7 | 9 7 6 | 7 6 4
-    // 3 2 6 | 2 6 9 | 6 9 1
-    // - - - - - - - - - - -
-    // 6 3 4 | 3 4 8 | 4 8 5
-    // 7 1 5 | 1 5 2 | 5 2 3
-    // 9 8 2 | 8 2 7 | 2 7 6
+    // Double check that we solve it and it returns 1
+    assert(solve(sudoku_base, 0));
+    print_sudoku(sudoku_base);
 
-
-    // assert(valid(sudoku_base));
-
-    char a[] = {1, 1, 0, 1, 1, 1, 0, 0, 1};
-    char b[] = {0, 1, 1, 1, 0, 1, 0, 1, 0};
-    char c[] = {1, 1, 1, 1, 1, 1, 1, 0, 1};
-    char d[] = {1, 1, 1, 1, 1, 1, 0, 1, 0};
-    char e[] = {0, 1, 1, 0, 1, 0, 1, 1, 1};
-    char f[] = {1, 0, 1, 0, 1, 1, 1, 1, 0};
-    char g[] = {0, 1, 0, 1, 1, 0, 1, 1, 1};
-    char h[] = {1, 0, 1, 1, 1, 1, 1, 1, 1};
-    char i[] = {1, 1, 1, 0, 0, 1, 1, 1, 1};
-
-    // assert(solve(sudoku_base, 0) == 1);
-    solve(sudoku_base, 0);
-    print_array(sudoku_base);
-
-    
+    system("PAUSE");
 }
