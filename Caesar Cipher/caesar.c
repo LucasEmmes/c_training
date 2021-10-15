8df321e7c70ffb605584fa767d2bfeb44cc36568
Lucas Emmes <79790657+LucasEmmes@users.noreply.github.com>
2021-10-15 23:32:50 +0200

    caesar.c can now encrypt messages

--- Caesar Cipher/caesar.c
+++ Caesar Cipher/caesar.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <assert.h>
 
+char alphabet[32] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ', ',', '.', '!', '?', '\n'};
+char shifted_alphabet[32] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ', ',', '.', '!', '?', '\n'};
+
 void shift_alphabet(char alph[], char n) {
 
     // Allocate enough mempry to hold what is shifted out the left side
@@ -28,68 +31,104 @@ void shift_alphabet(char alph[], char n) {
     free(temp);
 }
 
-char alphabet[32] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ', ',', '.', '!', '?', '\n'};
-char shifted[32] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ', ',', '.', '!', '?', '\n'};
+char translate_letter(char letter) {
+
+    // Find index of letter in correct alphabet
+    char index = 0;
+    while (alphabet[index] != letter)
+    {
+        index ++;
+    }
+    
+    return shifted_alphabet[index];
+
+}
 
 
+// TODO: take command line arguments
+// TODO: enable encryption and decryption
+// TODO: write output to file
 int main(int argc, char const *argv[]) {
+
+    char key = 5;
 
     // Open file
     FILE *fPointer;
     fPointer = fopen("cock.txt", "r");
 
-    // Find end
-    fseek(fPointer, 0L, SEEK_END);
-    // Save length
-    int input_length = ftell(fPointer);
-    printf("Length: %d\n", input_length);
-    // Go back to start
+    // Measure file to get accurate length, since fseek(fPointer, 0L, SEEK_END)
+    // somehow counts '\n' as two chars
+    int input_length = 0;
+    char temp[1];
+    while (!feof(fPointer))
+    {
+        fgets(temp, 2, fPointer);
+        input_length++;
+    }
+
+    input_length--;
     rewind(fPointer);
 
 
-    while (!feof(fPointer))
-    {
-        // count amount of characters manually
-    }
-    
-
-
-
-    // alloc space for string
+    // Alloc space for string
     char * text = malloc(sizeof(char) * input_length);
-    // read
+    char char_check;
+    // Read file again, transfer to text
     for (int i = 0; i < input_length; i++)
     {
-        text[i] = fgetc(fPointer);
-    }
-    
-    int nl_characters = 0;
-    for (int i = 0; i < input_length; i++)
-    {
-        if (text[i] == '\n')
-        {
-            nl_characters++;
+        char_check = fgetc(fPointer);
+        if ((char_check >= 65 && char_check <= 90) || char_check == 32 || char_check == 44 || char_check == 46 || char_check == 33 || char_check == 63 || char_check == 10) {
+            // valid
+        } else if (char_check >= 97 && char_check <= 122) {
+            // lowercase character
+            char_check = char_check - 32;
+        } else {
+            // invalid character, insert '?'
+            char_check = 63;
         }
-        
+
+        text[i] = char_check;
     }
 
-    input_length = input_length - nl_characters;
-
+    // Print text (for debug)
     for (int i = 0; i < input_length; i++)
     {
         printf("%c", text[i]);
     }
-    
-    printf("_");
-    
+    printf("\n\n");
 
 
-    // shift correct amount
+    // Shift correct amount
+    shift_alphabet(shifted_alphabet, key);
 
     // apply shifted alphabet to text
+    for (int i = 0; i < input_length; i++)
+    {
+        text[i] = translate_letter(text[i]);
+    }
+    
+    printf("_");
+    // Print text (for debug)
+    for (int i = 0; i < input_length; i++)
+    {
+        printf("%c", text[i]);
+    }
+    printf("_");
 
     // save to output file
 
+
+
+    // // Find end
+    // fseek(fPointer, 0L, SEEK_END);
+    // // Save length
+    // int input_length1 = ftell(fPointer);
+    // printf("Length 1: %d\n", input_length1);
+    // // Go back to start
+
+    
+
+    free(text);
     fclose(fPointer);
     return 0;
 }
--- Caesar Cipher/test.c
+++ Caesar Cipher/test.c
@@ -1,12 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+#include <sys/types.h>
+#include <regex.h>
 
+regex_t regex;
+int reti;
 
 int main(int argc, char const *argv[]) {
-
-    char a[] = {'\n'};
-
-    printf("%d", a[0]);
+    printf("pain");
+    reti = regcomp(&regex, "[A-Z]", 0);
     
+    if (!reti) {
+        printf("could not compile regex");
+        exit(1);
+    }
+
+    reti = regexec(&regex, "A", 0, NULL, 0);
+    if (!reti) {
+        printf("MATCH :)");
+    }
+    else if (reti == REG_NOMATCH) {
+        
+        printf("NO MATCH :(");
+    } else {
+        printf("regex match failed");
+    }
+
+
 }
