042e47cec53e2e0e4508c6a62a8febe5befbd8c7
Lucas Emmes <79790657+LucasEmmes@users.noreply.github.com>
2021-10-26 22:50:24 +0200

    Create dijkstra.c

---
+++ Dijkstra/dijkstra.c
@@ -0,0 +1,17 @@
+
+
+typedef struct {
+    char symbol;
+    node * shortest_path_via;
+    short shortest_path_length;
+    // edges
+} node;
+
+int main(int argc, char const *argv[])
+{
+    
+
+
+
+    return 0;
+}
