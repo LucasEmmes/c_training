82a8f669c666c2a2aae355cfd98baa3e5b8d88df
Lucas Emmes <79790657+LucasEmmes@users.noreply.github.com>
2021-10-28 17:51:01 +0200

    Update dijkstra.c
    Can now dynamically add nodes as edges to other nodes

--- Dijkstra/dijkstra.c
+++ Dijkstra/dijkstra.c
@@ -1,45 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 typedef struct node_t {
     char name;
     short shortest_path_length;
     char num_of_edges;
+    char active;
     struct node_t * shortest_path_via;
-    struct node_t * edges;
+    struct node_t * * edges;
+    char * weights;
 } node_t;
 
-void add_edge(node_t node1, node_t node2) {
-    char old_length = node1.num_of_edges;
+void add_edge(node_t * node1, node_t * node2, char weight) {
+    char old_length = node1->num_of_edges;
     char new_length = old_length + 1;
 
-    // node_t * new_edges = (struct node_t *) malloc(sizeof(node_t)*new_length);
+    node_t * * new_edges = (struct node_t * *) malloc(sizeof(node_t)*new_length);
+    char * new_weights = (char *) malloc(sizeof(char) * new_length);
+
+    for (char i = 0; i < new_length-1; i++)
+    {
+        new_edges[i] = node1->edges[i];
+        new_weights[i] = node1->weights[i];
+    }
+
+    new_edges[new_length-1] = node2;
+    new_weights[new_length-1] = weight;
+
+    // Free the old arrays
+    free(node1->edges);
+    free(node1->weights);
+
+    node1->edges = new_edges;
+    node1->weights = new_weights;
+    node1->num_of_edges = new_length;
 }
 
 void print_edges(node_t node) {
-    // for (char i = 0; i < node.num_of_edges; i++)
-    // {
-    //     printf("%c", node->edges[i]);
-    // }
-    
+    printf("Edges: %d\n", node.num_of_edges);
+    for (char i = 0; i < node.num_of_edges; i++)
+    {
+        printf("N:%c W:%d, ", node.edges[i]->name, node.weights[i]);
+    }
+    printf("\n");
 }
 
 int main(int argc, char const *argv[])
 {
-    
-    // printf("%d\n", sizeof(node_t));
-    // printf("%d\n", sizeof(node_t *));
 
     node_t a;
     a.name = 'a';
+
     node_t b;
     b.name = 'b';
 
-    node_t custom_edges[1] = {b};
-    a.edges = custom_edges;
+    add_edge(&a, &b, 5);
 
-    printf("%c", a.name);
-    printf("%c", a.edges[0].name);
+    print_edges(a);
+
 
     return 0;
 }
