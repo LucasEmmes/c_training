9330641e83c7d11813cc98eb88af74197fa454c6
Lucas Emmes <lucas.f.emmes@protonmail.com>
2022-02-15 23:59:13 +0100

    Started working on reading the input file

--- Better_dijkstra/dijkstra.c
+++ Better_dijkstra/dijkstra.c
@@ -8,8 +8,8 @@
 uint64_t uid_counter = 0;
 
 struct edge_t {
-    struct node_t * node;
     uint64_t cost;
+    struct node_t * node;
 };
 
 struct node_t {
@@ -23,7 +23,7 @@ struct node_t {
     struct edge_t ** edges;
 };
 
-struct node_t * initialize_node(char name[12]) {
+struct node_t * initialize_node(char * name) {
     // Make new node pointer
     struct node_t * node = malloc(sizeof(* node));
     uid_counter ++;
@@ -54,15 +54,15 @@ void add_edge(struct node_t * from, struct node_t * to, uint64_t cost) {
     // No more edges available, extend
     else if (from->num_of_actual_edges == from->num_of_edges_available) {
 
-        struct edge_t ** bigger_edge_array = malloc(sizeof(struct edge_t *) * from->num_of_edges_available*2);
+        struct edge_t ** bigger_edge_array = malloc(sizeof(struct edge_t *) * from->num_of_edges_available * 2);
         memcpy(bigger_edge_array, from->edges, sizeof(from->edges));
         free(from->edges);
         from->edges = bigger_edge_array;
-
+        from->num_of_edges_available = from->num_of_edges_available * 2;
     }
 
     // Make edge
-    struct edge_t * edge;
+    struct edge_t * edge = malloc(sizeof(* edge));
     edge->cost = cost;
     edge->node = to;
 
@@ -72,20 +72,53 @@ void add_edge(struct node_t * from, struct node_t * to, uint64_t cost) {
 }
 
 void print_edges(struct node_t * node) {
-    for (uint16_t i; i < node->num_of_actual_edges; i++) {
-        printf("%s", node->edges[i]->node->name);
+    for (uint16_t i = 0; i < node->num_of_actual_edges; i++) {
+        printf("%s\n", node->edges[i]->node->name);
     }
 }
 
+void read_node_name(char * name, char divc, FILE * fp) {
+    int counter = 0;
+    int current_char = fgetc(fp);
+    while (current_char != divc) {
+        name[counter] = current_char;
+        counter++;
+    }
+}
+
+void read_data(char * filename) {
+    FILE * fp = fopen("file.txt", "r");
+    assert(fp);
+    
+    // Set up variables
+    char name_start[12];
+    char name_goal[12];
+    memset(name_start, 0, sizeof(name_start));
+    memset(name_goal, 0, sizeof(name_goal));
+
+    // Read name of start node
+    read_node_name(name_start, ',', fp);
+
+    // Read name of goal node
+    read_node_name(name_goal, '\n', fp);
+
+
+    fclose(fp);
+}
+
+void dijkstra(struct node_t * start, struct node_t * goal) {
+
+
+
+
+
+}
 
 int main(int argc, char const *argv[]) {
 
-    struct node_t * a = initialize_node("A           ");
-    struct node_t * b = initialize_node("Hello World");
-
-    add_edge(a, b, 10);
-
-    print_edges(a);
+    struct node_t * a = initialize_node("A");
+    // printf("%s\n",a->name);
+    printf("%d\n", ',');
 
     return 0;
 }
